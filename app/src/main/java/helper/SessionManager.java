9fec1c55f2c01ff99a0f55655f176f77adfb5293
Abhinav <oct94abhi@gmail.com>
2015-06-12 15:52:59 +0530

    Checkpoint1 (excluding navbar and app bar)

--- app/src/main/java/helper/SessionManager.java
+++ app/src/main/java/helper/SessionManager.java
@@ -7,6 +7,11 @@ import android.util.Log;
 /**
  * Created by Abhinav on 6/1/2015.
  */
+
+/**
+ *  This class maintains session data across the app using the SharedPreferences.
+ *  We store a boolean flag isLoggedIn in shared preferences to check the login status.
+ */
 public class SessionManager {
     //Logcat TAG
     private static String TAG = SessionManager.class.getSimpleName();
@@ -22,24 +27,24 @@ public class SessionManager {
 
     //Shared preferences file name
     private static final String PREF_NAME = "EcomLogin";
-    private static final String LOGGEDIN="isLoggedIn";
+    private static final String LOGGEDIN = "isLoggedIn";
 
     public SessionManager(Context context) {
         this._context = context;
-        pref=_context.getSharedPreferences(PREF_NAME,PRIVATE_MODE);
-        editor=pref.edit();
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
     }
 
     public void setLogin(boolean isLoggedIn) {
-        editor.putBoolean(LOGGEDIN,isLoggedIn);
+        editor.putBoolean(LOGGEDIN, isLoggedIn);
 
         //commit changes
         editor.commit();
 
-        Log.d(TAG,"User login session modified!");
+        Log.d(TAG, "User login session modified!");
     }
 
     public boolean isLoggedIn() {
-        return pref.getBoolean(LOGGEDIN,false);
+        return pref.getBoolean(LOGGEDIN, false);
     }
 }
--- app/src/main/java/live/Abhinav/ecommerce/app/DashboardActivity.java
+++ app/src/main/java/live/Abhinav/ecommerce/app/DashboardActivity.java
@@ -1,39 +1,81 @@
 package live.Abhinav.ecommerce.app;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import helper.SQLiteHandler;
+import helper.SessionManager;
+
+import java.util.HashMap;
 
 
 public class DashboardActivity extends ActionBarActivity {
+    //Logcat TAG
+    private static final String TAG=DashboardActivity.class.getSimpleName();
+
+    private TextView txtName,txtEmail;
+    private Button btnLogout;
+    private SessionManager session;
+    private SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-    }
 
+        txtName= (TextView) findViewById(R.id.name);
+        txtEmail= (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
-        return true;
-    }
+        //Sqlite database handler
+        db=new SQLiteHandler(getApplicationContext());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //SessionManager
+        session = new SessionManager(getApplicationContext());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(!session.isLoggedIn()) {
+            logoutUser();
         }
 
-        return super.onOptionsItemSelected(item);
+        //fetch user details from SQLITE DB
+        HashMap<String,String> user  =db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        Log.d(TAG,user.toString());
+        //Display the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        //Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
     }
+
+    /**
+     * Function to logout the user. Will set isLoggedIn flag to fasle in sharedPreferences file.
+     * Clears the user data from sqlite  users table.
+     */
+    private void logoutUser() {
+        session.setLogin(false);
+        db.deleteUsers();
+
+        //Launching the login activity
+        Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
